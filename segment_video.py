3dcb87f866f013653c30c6556f563b2beb66141c
hrz <herz23@mails.tsinghua.edu.cn>
2024-09-05 15:23:47 +0800

    z

--- segment_video.py
+++ segment_video.py
@@ -1,6 +1,10 @@
 from ultralytics import YOLO
+import time
 
-model = YOLO("yolov8m.engine")
-results = model("256s.mp4", save=True, stream=True, classes=[32])
+model = YOLO("yolov8n-seg.pt")
+start_time = time.time()
+results = model("256s.mp4", stream=True)
 for result in results:
     pass
+end_time = time.time()
+print(f"推理时间：{(end_time - start_time):.2f}s")
--- streaming_for_loop.py
+++ streaming_for_loop.py
@@ -2,15 +2,14 @@ import cv2
 from ultralytics import YOLO
 import time
 
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8n-seg.pt")
 video_path = "256s.mp4"
 cap = cv2.VideoCapture(video_path)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc("M", "J", "P", "G"))
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-cap.set(cv2.CAP_PROP_FPS, 60)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 prev_time = time.time()
-fps = 0
+start_time = time.time()
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -19,7 +18,6 @@ while cap.isOpened():
         elapsed_time = current_time - prev_time
         prev_time = current_time
         fps = 1 / elapsed_time
-
         results = model(frame)
         annotated_frame = results[0].plot()
         cv2.putText(
@@ -31,12 +29,15 @@ while cap.isOpened():
             (0, 255, 0),
             2,
         )
-
         cv2.imshow("YOLOv8 Inference", annotated_frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
         break
-
+end_time = time.time()
 cap.release()
 cv2.destroyAllWindows()
+print(f"推理时间：{(end_time - start_time):.2f}s")
+"""
+pt 111s
+"""
