75072a9bc7c3f0fb7cb11bfe80aace8acb3dc844
hrz <herz23@mails.tsinghua.edu.cn>
2024-10-05 20:43:26 +0800

    1

--- streaming_for_loop.py
+++ streaming_for_loop.py
@@ -2,12 +2,12 @@ import cv2
 from ultralytics import YOLO
 import time
 
-model = YOLO("yolo11n.onnx")
+model = YOLO("yolo11x.onnx")
 video_path = "256s.mp4"
 cap = cv2.VideoCapture(0)
-# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc("M", "J", "P", "G"))
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc("M", "J", "P", "G"))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 prev_time = time.time()
 start_time = time.time()
 
@@ -18,10 +18,10 @@ while cap.isOpened():
         elapsed_time = current_time - prev_time
         prev_time = current_time
         fps = 1 / elapsed_time
-        # results = model(frame)
-        # annotated_frame = results[0].plot()
+        results = model(frame)
+        annotated_frame = results[0].plot()
         cv2.putText(
-            frame,
+            annotated_frame,
             f"FPS: {int(fps)}",
             (10, 30),
             cv2.FONT_HERSHEY_SIMPLEX,
@@ -29,7 +29,7 @@ while cap.isOpened():
             (0, 255, 0),
             2,
         )
-        cv2.imshow("YOLOv8 Inference", frame)
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
@@ -37,7 +37,3 @@ while cap.isOpened():
 end_time = time.time()
 cap.release()
 cv2.destroyAllWindows()
-print(f"推理时间：{(end_time - start_time):.2f}s")
-"""
-pt 52s
-"""
