b1d65800dd8a7d7762295103ec2eb149e740f933
hrz <herz23@mails.tsinghua.edu.cn>
2024-10-05 20:36:52 +0800

    2

--- export.py
+++ export.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")
-model.export(format="engine")
+model.export(format="engine", dynamic=True, simplify=True)
--- video.py
+++ video.py
@@ -3,15 +3,8 @@ import time
 
 model = YOLO("yolo11n.engine")
 start_time = time.time()
-results = model.predict("256s.mp4", save=True, stream=True)
+results = model.predict("256s.mp4", stream=True)
 for result in results:
     pass
 end_time = time.time()
 print(f"推理时间：{(end_time - start_time):.2f}s")
-
-
-"""
-pt 71s 110s
-engine 67s 82s
-engine int8 48s
-"""
